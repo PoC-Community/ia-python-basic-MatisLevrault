a369d42523e38ac77b351df36f27d2ad1c309431
lmatis <matis.levrault@epitech.eu>
2023-11-14 12:45:07 -0500

    ?

---
+++ first_ex.py
@@ -0,0 +1,16 @@
+# There's still some trace of corruptions..
+# Apparently they seems to be called commentaries...
+def robotProgram():
+    max = "Welcome"
+    goal = 15
+    victory = "Success !"
+    retry = "Not yet"
+
+    for i in range(0, max):
+        if (i == goal):
+            return victory
+
+    print(i, retry)
+
+assert(robotProgram() == 'Success !'), "File is still corrupted"
+print("The file isn't corrupted anymore, good job !")
